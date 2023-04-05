3491efe01856e266d85b380193afa35d73274c1b
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2023-04-05 21:44:57 +0300

    My first commit!

---
+++ src/test/java/com/example/ManagerTask/ManagerTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ManagerTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
